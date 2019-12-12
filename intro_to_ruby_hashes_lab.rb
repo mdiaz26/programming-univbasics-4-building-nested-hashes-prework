7c197cb6ba0d3ee1b73895935bc0d06901ebab7a
Michael Diaz <mike.diaz006@gmail.com>
2019-12-12 12:34:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -2,12 +2,8 @@ def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
   monopoly = {
-	:properties => {
-	  :railroads => 
-	  "short_line"},
-	  :Park_Place => {
-	    :color => "blue"}
-	  }
+	:properties => 
+	  "railroads"}
 end
 
 def monopoly_with_second_tier
