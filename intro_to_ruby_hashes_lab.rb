12aa251c60e03ae569451a393b716b74cc3181a1
Michael Diaz <mike.diaz006@gmail.com>
2019-12-12 12:48:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,7 @@ def monopoly_with_third_tier
 	  :pieces => 4,
 	  :rent_in_dollars => {
 	    :one_piece_owned => 25,
-	    :rent2 => 2,
+	    :two_pieces_owned => 50,
 	    :rent3 => 3,
 	    :rent4 => 4
 	  },
