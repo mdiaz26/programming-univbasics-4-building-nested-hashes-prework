5af781a8602d0773248b5426968ed5732e45ed04
Michael Diaz <mike.diaz006@gmail.com>
2019-12-12 12:20:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,9 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-  monopoly =  
-	{:category => "board game"}
-	monopoly[:first_published] = "1933"
+  monopoly[:first_published] = "1933"
+	{}
+	
 end
 
 def monopoly_with_second_tier
