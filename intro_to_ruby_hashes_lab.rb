d598600b7d5c96e1620166fb724dc353c5c14d8a
Michael Diaz <mike.diaz006@gmail.com>
2019-12-12 12:16:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -2,7 +2,7 @@ def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
   monopoly =  
-	{category: "board game", }
+	{category: "board game", first published: "1933"}
 end
 
 def monopoly_with_second_tier
